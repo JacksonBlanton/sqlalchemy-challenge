60c78f5f30a73194d5695060cbeedf3b31d172b3
Jackson Blanton <Jackson@Jacksons-Air.lan>
2023-01-13 15:40:40 -0600

    added app.py file

---
+++ SurfsUp/app.py
@@ -0,0 +1,12 @@
+pip install sqlalchemy
+
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask
